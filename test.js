4ec104d4eb649d00730459dbe2ed0a1bd0893da7
FarrukhSajjad46 <farrukhsajjad46@gmail.com>
2023-09-13 10:47:01 +0500

    Boom

---
+++ test.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log(" Do Good have Good ");
+let saleem = " A true Friend ";
+let salary = 20000;
+let employed = true;
+console.log(saleem);
+console.log(salary);
+console.log(employed);
---
+++ test.ts
@@ -0,0 +1,7 @@
+console.log(" Do Good have Good ");
+let saleem:string = " A true Friend ";
+let salary:number =  20000;
+let employed:boolean = true;
+console.log(saleem);
+console.log(salary);
+console.log(employed);
